114f6ff0fe7fbe7c9132b9f83b0d437521ff397c
NOHWONDOO <studycloudera01@gmail.com>
2021-07-01 15:43:40 +0900

    Update docker

---
+++ freeboard_mobile/App.tsx
@@ -0,0 +1,34 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import {WebView} from 'react-native-webview';
+
+
+const App = () => {
+  return <WebView source={{ uri: 'https://codebootcamp.co.kr' }}/>
+};
+
+export default App;
